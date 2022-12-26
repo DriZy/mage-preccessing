8ad7bdc173762c8748580b80c48a41ae2ab19511
Tabi Idris <tabidrizy@gmail.com>
2022-12-26 05:57:41 +0100

    fixed route-log directory error

--- src/controllers/logger.ts
+++ src/controllers/logger.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import {promises as fsPromises} from "fs";
 import path from "path";
+import fs from "fs";
 
 /**
  * Application logger for all system event
@@ -8,8 +9,13 @@ import path from "path";
  * @param res
  * */
 const logger = async (req: express.Request, res: express.Response, next: Function) => {
+    //check if output directory exist already
+
     const baseUrl = path.resolve('src')
     const logsDirectory = path.resolve(baseUrl, 'route-logs')
+    if (!fs.existsSync(logsDirectory)) {
+        fs.mkdirSync(logsDirectory);
+    }
     const date_ob = new Date();
     const day = ("0" + date_ob.getDate()).slice(-2);
     const month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
